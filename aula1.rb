3cbe81b9291018f58afe4ad78ec56e8581508750
fariasvtr <fariasvtr@outlook.com>
2024-02-28 07:56:45 -0300

    inserção de primeira aula e projeto 1

---
+++ aula1.rb
@@ -0,0 +1,4 @@
+puts "Olá Usuario"
+print "Qual o seu nome? "
+nome = gets.chomp
+puts "Bem vindo #{nome}"
---
+++ projeto01.rb
@@ -0,0 +1,11 @@
+puts "Digite seu nome: "
+nome = gets.chomp
+
+puts "Digite seu sobrenome: "
+sobrenome = gets.chomp
+
+puts "Digite a sua idade: "
+idade = gets.chomp
+
+print "Seja muito bem-vindo #{nome} #{sobrenome}, voce tem #{idade}
+e pode desfrutar de nossa biblioteca!"
